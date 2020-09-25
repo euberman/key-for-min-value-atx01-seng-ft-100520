b1aabf7c2c3036e74f2b46dc5b7177dbf3673647
Eric Uberman <euberman@gmail.com>
2020-09-25 23:07:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_key = ""
+  if name_hash.empty?
+    return nil
+  end
   name_hash.each do |key,value|
-    if name_hash.empty?
-      return nil
-    elsif min_key.empty?
+    if min_key.empty?
       min_key = key
     else name_hash[key] < name_hash[minKey]
       min_key = key
     end
   end
-  min_key
+  return min_key
 end
