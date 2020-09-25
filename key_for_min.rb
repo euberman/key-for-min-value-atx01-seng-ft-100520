4642eeaeec5a74d3cd84307db389ed38472160cf
Eric Uberman <euberman@gmail.com>
2020-09-25 23:15:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_key = ""
+  
   if name_hash.empty?
     return nil
   end
+  
   name_hash.each do |key,value|
     if min_key.empty?
       min_key = key
@@ -13,5 +15,6 @@ def key_for_min_value(name_hash)
       min_key = key
     end
   end
+  
   return min_key
 end
